38c5c7a8eeb7b1a6e4137fe6153318a64c408480
Ghaith shuraiki <gshuraiki@gmail.com>
2018-01-21 03:26:03 +0100

    Create week02 home work.js

---
+++ week02 home work.js
@@ -0,0 +1,69 @@
+let ar = "مرحبا"
+let en =" Hello"
+let nl = "Dag"
+let es = " Hola"
+console.log(ar, en, nl, es)
+console.log("'I'm awesome'");
+let x
+console.log('the value of my variable x will be: Not defined')
+console.log(x)
+x = 10
+console.log('the value of my variable x will be: 10')
+console.log("x value is", x)
+let y = "Ghaith"
+console.log('the value of my string will be: Ghaith');
+console.log("y value is", y)
+y = "shuraiki"
+console.log('the value of my string will be: shuraiki');
+console.log("y value is", y)
+let z = 7.25
+console.log("z =", z)
+let a = 7
+console.log("a =", a)
+if (a > z) { console.log("the hight value is a=",a) }
+else console.log("the hight value is z=",z)
+let plurality = []
+console.log('the value of my variable plurality will be: Not defined')
+console.log(plurality)
+plurality = ["dog", "cat", "snake"]
+console.log(plurality)
+let newLength = plurality.push("baby big")
+console.log(plurality)
+let myString = "this is a test"
+console.log(myString)
+console.log("the length of myString is ", myString.length)
+let q = 1
+let w = "2"
+let e = true
+let r = []
+console.log('The value of my variable foo is: ',q,"number")
+console.log('The value of my variable foo is: ',w,"string")
+console.log('The value of my variable foo is: ', e,"Booleans")
+console.log('The value of my variable foo is: ',r,"array")
+console.log(typeof q)
+console.log(typeof w)
+console.log(typeof e)
+console.log(typeof r)
+if (typeof q === typeof w || typeof q === typeof e || typeof q === typeof r
+    || typeof w === typeof e || typeof w === typeof r || typeof e === typeof r) {
+    console.log("same type")
+}
+else console.log("not same type")
+x = 7
+x = x % 3
+console.log(x)
+x = 7
+x = x % 7
+console.log(x)
+x = 100
+x = x % 2
+console.log(x)
+
+//10
+let myArray = [1, "hi", true]
+console.log("yes i Can  store multiple types in an array")
+let c = 6 / 0
+let b = 10 / 0
+if (c === b) { console.log("yes we can compare infinities") }
+else console.log("we can not compare")    
+
