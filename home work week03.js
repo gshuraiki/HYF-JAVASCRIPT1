6f9e45dfa369fc73dd446afc2d9a16f5733c2e14
Ghaith shuraiki <gshuraiki@gmail.com>
2018-01-23 23:24:41 +0100

    Update home work week03.js

--- home work week03.js
+++ home work week03.js
@@ -13,4 +13,16 @@ console.log(favoriteAnimals);
 console.log("my array length: ", favoriteAnimals.length);
 favoriteAnimals.splice(3, 1);
 console.log(favoriteAnimals);
-console.log("The item you are looking for is at index: ",favoriteAnimals.indexOf("meerkat"))
+console.log("The item you are looking for is at index: ", favoriteAnimals.indexOf("meerkat"));
+//More JavaScript 1
+function sum(a, b, c) {
+    console.log("your sum function value" , arguments[0], "+", arguments[1], "+", arguments[2], "=");
+    return (a + b + c)
+};
+console.log(sum(12, 434, 656))
+function colorCar(d) {
+    console.log("chose you car color ");
+    console.log("you have chose a ", arguments[0], " color");
+    return (d)
+};
+console.log("you receive a",colorCar("red"),"car")
